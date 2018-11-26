6e8c83d8bda2915cb98d0e796dfc8233c11b7e3f
howie <l.ahao0705@gmail.con>
2018-11-26 23:25:10 +0800

    今天到这

---
+++ 15/15.go
@@ -0,0 +1,76 @@
+/*
+给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+
+满足要求的三元组集合为：
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+package threeSum
+
+type TreeNode struct {
+	Val       int
+	LeftNode  *TreeNode
+	RightNode *TreeNode
+}
+
+//FIXME 递归写错了
+func (node *TreeNode) toString() string {
+	var s string
+
+	if node.LeftNode != nil {
+		s += node.LeftNode.toString() + "<"
+	} else {
+		return ""
+	}
+	if node != nil {
+		s += node.toString() + "<"
+	} else {
+		return ""
+	}
+	if node.RightNode != nil {
+		s += node.RightNode.toString() + "<"
+	} else {
+		return ""
+	}
+	return s
+}
+func (node *TreeNode) add(v int) {
+	if node.Val >= v {
+		if node.RightNode == nil {
+			node.RightNode = &TreeNode{
+				Val: v,
+			}
+		} else {
+			node.RightNode.add(v)
+		}
+	} else {
+		if node.LeftNode == nil {
+			node.LeftNode = &TreeNode{
+				Val: v,
+			}
+		} else {
+			node.LeftNode.add(v)
+		}
+	}
+}
+
+// 构建根节点为0的二叉搜索树
+func buildTree(nodes []int) *TreeNode {
+	var root = &TreeNode{
+		Val: 0,
+	}
+	for index := range nodes {
+		root.add(index)
+	}
+	return root
+}
+
+// func threeSum(nums []int) [][]int {
+
+// }
---
+++ 15/15_test.go
@@ -0,0 +1,12 @@
+package threeSum
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTreeNode(t *testing.T) {
+	numbs := [4]int{1, 2, 3, 4}
+	fmt.Printf(buildTree(numbs[:]).toString())
+
+}
