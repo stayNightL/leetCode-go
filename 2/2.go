ad436e8daa9d6e2ed83b598bf13011a27fe3edd1
howie <l.ahao0705@gamil.com>
2018-12-18 21:32:34 +0800

    还有问题

---
+++ 2/2.go
@@ -0,0 +1,85 @@
+/*
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+*/
+
+package addTwoNumbers
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//TODO 有问题
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	list := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+	head := list
+	for {
+
+		if l1 != nil {
+			list.Val = l1.Val + list.Val
+			l1 = l1.Next
+
+		}
+
+		if l2 != nil {
+			list.Val = l2.Val + list.Val
+			l2 = l2.Next
+
+		}
+
+		if l1 == nil && l2 == nil {
+			break
+		}
+
+		list.Next = &ListNode{
+			Val:  0,
+			Next: nil,
+		}
+		list = list.Next
+	}
+
+	isNeedAdd := false
+	list.Next = &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+	node := head
+	p := head
+	for {
+
+		if isNeedAdd {
+			node.Val = head.Val + 1
+
+		}
+		if node.Val >= 10 {
+			isNeedAdd = true
+			node.Val = node.Val - 10
+		} else {
+			isNeedAdd = false
+		}
+		if node.Next == nil {
+
+			break
+		}
+		p = node
+		node = node.Next
+	}
+	if node.Val == 0 {
+		p.Next = nil
+	}
+	return head
+}
---
+++ 2/2_test.go
@@ -0,0 +1,30 @@
+package addTwoNumbers
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	l1 := &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val: 4,
+			Next: &ListNode{
+				Val:  3,
+				Next: nil,
+			},
+		},
+	}
+	l2 := &ListNode{
+		Val: 5,
+		Next: &ListNode{
+			Val: 6,
+			Next: &ListNode{
+				Val:  4,
+				Next: nil,
+			},
+		},
+	}
+	addTwoNumbers(l1, l2)
+
+}
